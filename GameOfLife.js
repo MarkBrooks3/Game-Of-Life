608346b98542d54cf358c04c78812beecaa34024
MarkBrooks3 <78883486+MarkBrooks3@users.noreply.github.com>
2021-02-15 16:27:12 -0600

    cleaner code

--- GameOfLife.js
+++ GameOfLife.js
@@ -1,64 +1,64 @@
 class GameOfLife {
-    constructor(width,height) {
-        this.width = width;
-        this.height = height;
-        this.board = this.makeBoard();
-    }
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+    this.board = this.makeBoard();
+  }
 
-    makeBoard() {
-        board = []
-        for(let i = 0; i < this.height; i++) {
-            let row = []
-            for(let j = 0; j < this.width; j++) {
-                row.push(0)
-            }
-            board.push(row);
+  makeBoard() {
+    board = [];
+    for (let i = 0; i < this.height; i++) {
+      let row = [];
+      for (let j = 0; j < this.width; j++) {
+        row.push(0);
+      }
+      board.push(row);
+    }
+    return board;
+  }
+
+  getCell(row, col) {
+    if (row >= this.height || col >= this.width || row < 0 || col < 0) {
+      return 0;
+    } else {
+      return this.board[row][col];
+    }
+  }
+
+  toggleCell(row, col) {
+    this.board[row][col] = 1 - this.getCell(row, col);
+  }
+
+  checkNeighbors(row, col) {
+    let livingNeighbors = 0;
+    for (let i = row - 1; i <= row + 1; i++) {
+      for (let j = col - 1; j <= col + 1; j++) {
+        if (this.getCell(i, j) === 1) {
+          livingNeighbors += 1;
         }
-        return board
+      }
     }
+    return livingNeighbors - this.board[row][col];
+  }
 
-    getCell(row,col) {
-        if(row >= this.height || col >= this.width || row < 0 || col < 0) {
-            return 0
+  playGame() {
+    const newBoard = this.makeBoard();
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        let neighbors = this.checkNeighbors(i, j);
+        if (this.getCell(i, j) === 0) {
+          if (neighbors === 3) {
+            newBoard[i][j] = 1;
+          }
         } else {
-            return this.board[row][col];
+          if (neighbors < 2 || neighbors > 3) {
+            newBoard[i][j] = 0;
+          } else {
+            newBoard[i][j] = 1;
+          }
         }
+      }
     }
-
-    toggleCell(row,col) {
-        this.board[row][col] = 1 - this.getCell(row,col);
-    }
-
-    checkNeighbors(row,col) {
-        let livingNeighbors = 0
-        for(let i = row - 1; i <= row + 1; i++) {
-            for(let j = col - 1; j <= col + 1; j++) {
-                if(this.getCell(i,j) === 1) {
-                    livingNeighbors += 1
-                }
-            }
-        }
-        return livingNeighbors - this.board[row][col]
-    }
-
-    playGame() {
-        const newBoard = this.makeBoard()
-        for(let i = 0; i < this.height; i++) {
-            for (let j = 0; j < this.width; j++) {
-                let neighbors = this.checkNeighbors(i,j)
-                if(this.getCell(i,j)=== 0){
-                    if(neighbors === 3) {
-                        newBoard[i][j] = 1
-                    }
-                } else {
-                    if(neighbors < 2 || neighbors > 3) {
-                        newBoard[i][j] = 0
-                    } else {
-                        newBoard[i][j] = 1
-                    }
-                }
-            }
-        }
-        this.board = newBoard
-    }
+    this.board = newBoard;
+  }
 }
